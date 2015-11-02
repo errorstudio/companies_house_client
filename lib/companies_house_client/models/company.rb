905b1118080ef9abe6afcd8c405f6ec3c05c96ec
Ed Jones <ed@errorstudio.co.uk>
2015-11-02 20:00:02 +0000

    Add Company.search() which takes an argument for the search term, and optionally per_page and start

--- lib/companies_house_client/models/company.rb
+++ lib/companies_house_client/models/company.rb
@@ -4,12 +4,24 @@ module CompaniesHouseClient
     collection_path "/company"
     primary_key :company_number
 
+    def self.search(term, per_page: nil, start: nil)
+      get("/search",{q: term, items_per_page: per_page, start_index: start})
+    end
+
     def name
       company_name
     end
 
     def number
       company_number
+    end
+
+    def active?
+      company_status == "active"
+    end
+
+    def dissolved?
+      !active?
     end
   end
 end
